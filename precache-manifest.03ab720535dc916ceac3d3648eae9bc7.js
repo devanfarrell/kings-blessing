d49797e9becfb6c8910ff26b3389136786bc93b7
Devan Farrell <devan.farrell16@gmail.com>
2020-02-24 23:05:21 -0800

    Updates

---
+++ precache-manifest.03ab720535dc916ceac3d3648eae9bc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5318bd74747bce7252363a8d4fd81413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80a93112823ee1bc18f",
+    "url": "/static/css/2.f7347798.chunk.css"
+  },
+  {
+    "revision": "f80a93112823ee1bc18f",
+    "url": "/static/js/2.466639d1.chunk.js"
+  },
+  {
+    "revision": "bd627f3ce823080daebae8d5e2c1a3bb",
+    "url": "/static/js/2.466639d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3dd1fb420d63c908d4",
+    "url": "/static/js/main.e41f4a7f.chunk.js"
+  },
+  {
+    "revision": "2fbca805cacc21f0e74d",
+    "url": "/static/js/runtime-main.931197c8.js"
+  },
+  {
+    "revision": "8e3d57bece2859097228c058c2a3e717",
+    "url": "/static/media/kings_blessing_click.8e3d57be.wav"
+  },
+  {
+    "revision": "6c497ffb2917e69207a893d869e7bc6c",
+    "url": "/static/media/kings_blessing_fail.6c497ffb.wav"
+  },
+  {
+    "revision": "c54374c9b156741d58156235557715d7",
+    "url": "/static/media/kings_blessing_success.c54374c9.wav"
+  }
+]);
